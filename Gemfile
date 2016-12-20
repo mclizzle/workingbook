4d2fb85f8be110742ed5550480248caed9bd4813
elizabethamckenna <elizabeth.a.mckenna@gmail.com>
2016-12-20 12:17:50 -0500

    seeding database

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'gravatarify'
 gem 'twitter-bootstrap-rails'
 gem 'simple_form'
 gem 'devise'
+gem 'seed_dump'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-5.times do |x|
-	User.create(username: "awesomeStudent#{x}", email: "student#{x}@gmail.com", password: "password", password_confirmation: "password")
-end
-p "Test users created"
+PublicActivity::ORM::ActiveRecord::Activity.create!([
+  {trackable_type: "Friendship", trackable_id: 8, owner_type: "User", owner_id: 3, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 8, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 3},
+  {trackable_type: "Friendship", trackable_id: 8, owner_type: "User", owner_id: 3, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 8, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 3},
+  {trackable_type: "Friendship", trackable_id: 8, owner_type: "User", owner_id: 3, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 8, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 3},
+  {trackable_type: "Friendship", trackable_id: 9, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 9, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 9, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 9, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 7, owner_type: "User", owner_id: 3, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 7, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 3},
+  {trackable_type: "Friendship", trackable_id: 7, owner_type: "User", owner_id: 3, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 7, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 3},
+  {trackable_type: "Friendship", trackable_id: 10, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 10, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 10, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 10, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 11, owner_type: "User", owner_id: 3, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 11, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 3},
+  {trackable_type: "Friendship", trackable_id: 11, owner_type: "User", owner_id: 3, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 11, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 3},
+  {trackable_type: "Friendship", trackable_id: 12, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 12, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 13, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 4},
+  {trackable_type: "Friendship", trackable_id: 13, owner_type: "User", owner_id: 4, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 13, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 4},
+  {trackable_type: "Friendship", trackable_id: 13, owner_type: "User", owner_id: 4, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 14, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 3},
+  {trackable_type: "Friendship", trackable_id: 14, owner_type: "User", owner_id: 3, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 16, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Friendship", trackable_id: 16, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 17, owner_type: "User", owner_id: 1, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 2},
+  {trackable_type: "Friendship", trackable_id: 17, owner_type: "User", owner_id: 2, key: "friendship.accepted", parameters: {}, recipient_type: "User", recipient_id: 1},
+  {trackable_type: "Post", trackable_id: 3, owner_type: "User", owner_id: 1, key: "post.created", parameters: {}, recipient_type: nil, recipient_id: nil},
+  {trackable_type: "Post", trackable_id: 4, owner_type: "User", owner_id: 1, key: "post.created", parameters: {}, recipient_type: nil, recipient_id: nil}
+])
+User.create!([
+  {email: "bartdn183@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 11, current_sign_in_at: "2016-10-21 23:05:32", last_sign_in_at: "2016-10-21 18:28:33", current_sign_in_ip: "::1", last_sign_in_ip: "::1", username: "bartdn183", name: "Bart De Nef", age: 33, bio: "TTS Code Immersion Instructor", school: "Tech Talent South", work: "Full Time Coder"},
+  {email: "student0@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 5, current_sign_in_at: "2016-10-21 18:26:45", last_sign_in_at: "2016-10-21 18:24:37", current_sign_in_ip: "::1", last_sign_in_ip: "::1", username: "awesomeStudent0", name: nil, age: nil, bio: nil, school: nil, work: nil},
+  {email: "student1@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 6, current_sign_in_at: "2016-10-21 18:21:05", last_sign_in_at: "2016-10-21 18:01:57", current_sign_in_ip: "::1", last_sign_in_ip: "::1", username: "awesomeStudent1", name: nil, age: nil, bio: nil, school: nil, work: nil},
+  {email: "student2@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2016-10-21 18:16:42", last_sign_in_at: "2016-10-21 18:16:42", current_sign_in_ip: "::1", last_sign_in_ip: "::1", username: "awesomeStudent2", name: nil, age: nil, bio: nil, school: nil, work: nil},
+  {email: "student3@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "awesomeStudent3", name: nil, age: nil, bio: nil, school: nil, work: nil},
+  {email: "student4@gmail.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "awesomeStudent4", name: nil, age: nil, bio: nil, school: nil, work: nil}
+])
+Friendship.create!([
+  {user_id: 1, friend_id: 6, state: "active", friended_at: "2016-10-19 21:55:43"},
+  {user_id: 3, friend_id: 2, state: "active", friended_at: "2016-10-21 17:21:08"},
+  {user_id: 3, friend_id: 1, state: "pending", friended_at: nil},
+  {user_id: 1, friend_id: 2, state: "active", friended_at: "2016-10-21 18:26:51"},
+  {user_id: 1, friend_id: 4, state: "pending", friended_at: nil}
+])
+Post.create!([
+  {user_id: 1, content: "This is post nr.1"},
+  {user_id: 1, content: "post nr .4"}
+])
